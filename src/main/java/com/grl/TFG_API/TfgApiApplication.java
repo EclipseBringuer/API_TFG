9d47000f351a825b3f119f8e12808d547636fa75
gabri <gabrielrl2004@gmail.com>
2024-03-20 22:28:54 +0100

    Primer commit

---
+++ src/main/java/com/grl/TFG_API/TfgApiApplication.java
@@ -0,0 +1,13 @@
+package com.grl.TFG_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgApiApplication.class, args);
+	}
+
+}
