bc28316bfe676691b6399a0ddb1e52c471bb871a
gabri <gabrielrl2004@gmail.com>
2024-03-25 16:56:02 +0100

    Reubicación de la clase test

--- src/main/java/com/grl/TFG_API/controllers/TestController.java
+++ src/main/java/com/grl/TFG_API/controllers/TestController.java
@@ -1,4 +1,4 @@
-package com.grl.TFG_API;
+package com.grl.TFG_API.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
