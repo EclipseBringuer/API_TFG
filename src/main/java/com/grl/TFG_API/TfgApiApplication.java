ef456397b964a6b4d876e26c6b0336266c13b79f
gabri <gabrielrl2004@gmail.com>
2024-03-25 13:54:08 +0100

    Modificación de la App para que arranque la documentación

--- src/main/java/com/grl/TFG_API/TfgApiApplication.java
+++ src/main/java/com/grl/TFG_API/TfgApiApplication.java
@@ -1,13 +1,21 @@
 package com.grl.TFG_API;
 
+import com.grl.TFG_API.utilities.Utility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal que inicia la aplicación Spring Boot y abre la documentación en un navegador web.
+ */
 @SpringBootApplication
 public class TfgApiApplication {
 
+	/**
+	 * Método principal que inicia la aplicación Spring Boot y abre la documentación en un navegador web.
+	 * @param args Argumentos de línea de comandos.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TfgApiApplication.class, args);
+		Utility.openDocumentation();
 	}
-
 }
