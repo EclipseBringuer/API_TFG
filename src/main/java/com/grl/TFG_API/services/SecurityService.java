5d536e8c133e330f90e8877dffa3791891d4171f
gabri <gabrielrl2004@gmail.com>
2024-04-08 19:52:37 +0200

    Token de seguridad y metodo de validación de token creado

--- src/main/java/com/grl/TFG_API/services/SecurityService.java
+++ src/main/java/com/grl/TFG_API/services/SecurityService.java
@@ -1,7 +1,13 @@
 package com.grl.TFG_API.services;
 
 import org.springframework.stereotype.Service;
+import java.util.Objects;
 
 @Service
 public class SecurityService {
+    private static final String SECURITY_TOKEN = "15112004";
+
+    public static boolean isTokenValid(String token) {
+        return Objects.equals(token, SECURITY_TOKEN);
+    }
 }
