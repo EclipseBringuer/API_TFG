7a85ea501e354f01fecfd8a75b2b355a83dc909d
gabri <gabrielrl2004@gmail.com>
2024-05-12 17:25:01 +0200

    Implementación del OrderService

--- src/main/java/com/grl/TFG_API/services/OrderService.java
+++ src/main/java/com/grl/TFG_API/services/OrderService.java
@@ -1,15 +1,31 @@
 package com.grl.TFG_API.services;
 
+import com.grl.TFG_API.model.dto.NewOrderDTO;
+import com.grl.TFG_API.model.entity.Order;
 import com.grl.TFG_API.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
-    private OrderRepository repository;
+    private final OrderRepository repository;
 
     @Autowired
     public OrderService(OrderRepository repository) {
         this.repository = repository;
+    }
+
+    public Order createNewOrder(NewOrderDTO newOrderDTO) {
+        return repository.save(convertOrderDTOToOrder(newOrderDTO));
+    }
+
+    private Order convertOrderDTOToOrder(NewOrderDTO newOrderDTO) {
+        Order output = new Order();
+        output.setId(newOrderDTO.id());
+        output.setItems(newOrderDTO.items());
+        output.setPrice(newOrderDTO.price());
+        output.setUser(newOrderDTO.user());
+        output.setPaymentMethod(newOrderDTO.paymentMethod());
+        return output;
     }
 }
