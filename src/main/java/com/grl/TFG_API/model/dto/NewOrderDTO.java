88cb297e85b2c47b011f5e259a97e2cc8d0fd6be
gabri <gabrielrl2004@gmail.com>
2024-05-14 15:35:25 +0200

    Solución al fallo de compilación

--- src/main/java/com/grl/TFG_API/model/dto/NewOrderDTO.java
+++ src/main/java/com/grl/TFG_API/model/dto/NewOrderDTO.java
@@ -2,7 +2,6 @@ package com.grl.TFG_API.model.dto;
 
 import com.grl.TFG_API.model.entity.Item;
 import com.grl.TFG_API.model.entity.User;
-import com.grl.TFG_API.model.enums.PaymentMethod;
 import java.util.List;
 
 public record NewOrderDTO(Integer id, Double price, String paymentMethod, List<Item> items, User user) {
