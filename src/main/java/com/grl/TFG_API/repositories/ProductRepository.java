bfb484479c522626c6d6951428ccad7c0a9af050
gabri <gabrielrl2004@gmail.com>
2024-05-24 18:30:26 +0200

    Repositorio de product comentado con javadoc

--- src/main/java/com/grl/TFG_API/repositories/ProductRepository.java
+++ src/main/java/com/grl/TFG_API/repositories/ProductRepository.java
@@ -6,8 +6,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
+/**
+ * Interfaz del repositorio para gestionar entidades {@link Product}.
+ * <p>
+ * Esta interfaz extiende JpaRepository para proporcionar operaciones CRUD para las entidades Product.
+ * </p>
+ */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    /**
+     * Obtiene una lista de categorías distintas de productos.
+     *
+     * @return Lista de categorías distintas de productos.
+     */
     @Query("SELECT distinct(p.category) FROM Product p ORDER BY p.category")
     List<String> getDistinctCategories();
 }
