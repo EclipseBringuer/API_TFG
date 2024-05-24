19668c9f152210700af139150a1d2c31e9fff4e8
gabri <gabrielrl2004@gmail.com>
2024-05-24 18:08:27 +0200

    Main comentado

--- src/main/java/com/grl/TFG_API/TfgApiApplication.java
+++ src/main/java/com/grl/TFG_API/TfgApiApplication.java
@@ -1,6 +1,6 @@
 package com.grl.TFG_API;
 
-import com.grl.TFG_API.utilities.Utility;
+//import com.grl.TFG_API.utilities.Utility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,11 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TfgApiApplication {
 
     /**
-     * Método principal que inicia la aplicación Spring Boot y abre la documentación en un navegador web.
+     * Método principal que inicia la aplicación Spring Boot.
+     * <p>
+     * Este método ejecuta la aplicación utilizando SpringApplication.run y puede abrir la documentación en un navegador web si se descomenta la línea correspondiente.
+     * </p>
      *
      * @param args Argumentos de línea de comandos.
      */
     public static void main(String[] args) {
         SpringApplication.run(TfgApiApplication.class, args);
+        //Utility.openDocumentation();
     }
 }
