0b48e6fdb55ca04e40449c7b7b07841c4a4d8730
gabri <gabrielrl2004@gmail.com>
2024-05-24 18:31:15 +0200

    Repositorio de item comentado con javadoc

--- src/main/java/com/grl/TFG_API/repositories/ItemRepository.java
+++ src/main/java/com/grl/TFG_API/repositories/ItemRepository.java
@@ -4,6 +4,12 @@ import com.grl.TFG_API.model.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Interfaz del repositorio para gestionar entidades {@link Item}.
+ * <p>
+ * Esta interfaz extiende JpaRepository para proporcionar operaciones CRUD para las entidades Item.
+ * </p>
+ */
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
