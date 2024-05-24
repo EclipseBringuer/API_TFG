4d9b1d477f818870e86a8fe00c1b1b5b556b4650
gabri <gabrielrl2004@gmail.com>
2024-05-24 18:07:09 +0200

    Order repository comentado con javadoc

--- src/main/java/com/grl/TFG_API/repositories/OrderRepository.java
+++ src/main/java/com/grl/TFG_API/repositories/OrderRepository.java
@@ -10,13 +10,45 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * Interfaz del repositorio para gestionar entidades {@link Order}.
+ * <p>
+ * Esta interfaz extiende JpaRepository para proporcionar operaciones CRUD para las entidades Order.
+ * </p>
+ */
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    /**
+     * Recupera una lista de pedidos por el ID del usuario.
+     *
+     * @param id el ID del usuario.
+     * @return una lista de pedidos asociados con el ID del usuario especificado.
+     */
     List<Order> getOrderByUserId(Integer id);
 
+    /**
+     * Recupera una lista de pedidos que no están completados ni cancelados.
+     * <p>
+     * Este método utiliza una consulta JPQL personalizada para obtener pedidos
+     * donde el estado no es 'COMPLETED' ni 'CANCELED'.
+     * </p>
+     *
+     * @return una lista de pedidos que no están completados ni cancelados.
+     */
     @Query("SELECT o FROM Order o WHERE o.state NOT IN ('COMPLETED', 'CANCELED')")
     List<Order> getAllNotCompleted();
 
+    /**
+     * Actualiza el estado de un pedido por su ID.
+     * <p>
+     * Este método utiliza una consulta JPQL personalizada para actualizar el estado de un pedido
+     * identificado por su ID. El método está anotado con @Modifying y @Transactional
+     * para permitir la ejecución de la operación de actualización.
+     * </p>
+     *
+     * @param id    el ID del pedido a actualizar.
+     * @param state el nuevo estado a establecer.
+     */
     @Modifying
     @Transactional
     @Query("UPDATE Order o SET o.state = :state WHERE o.id = :id")
