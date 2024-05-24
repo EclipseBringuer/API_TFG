6a2fdc32cc4a058a8959e67b5ef5c48b1878ce0b
gabri <gabrielrl2004@gmail.com>
2024-05-24 18:22:51 +0200

    Servicio de item comentado con javadoc

--- src/main/java/com/grl/TFG_API/services/ItemService.java
+++ src/main/java/com/grl/TFG_API/services/ItemService.java
@@ -4,10 +4,18 @@ import com.grl.TFG_API.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Servicio para la gestión de items.
+ */
 @Service
 public class ItemService {
-    private ItemRepository repository;
+    private final ItemRepository repository;
 
+    /**
+     * Constructor del servicio ItemService.
+     *
+     * @param repository    El repositorio de items.
+     */
     @Autowired
     public ItemService(ItemRepository repository) {
         this.repository = repository;
