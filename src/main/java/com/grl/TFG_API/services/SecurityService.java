370a33e3e9d0fa5643b0950f6b9fb7db32dc49e0
gabri <gabrielrl2004@gmail.com>
2024-05-24 18:14:11 +0200

    Servicio de seguridad comentado con javadoc

--- src/main/java/com/grl/TFG_API/services/SecurityService.java
+++ src/main/java/com/grl/TFG_API/services/SecurityService.java
@@ -3,10 +3,19 @@ package com.grl.TFG_API.services;
 import org.springframework.stereotype.Service;
 import java.util.Objects;
 
+/**
+ * Servicio de seguridad para validar tokens de autenticación.
+ */
 @Service
 public class SecurityService {
     private static final String SECURITY_TOKEN = "15112004";
 
+    /**
+     * Verifica si un token proporcionado es válido.
+     *
+     * @param token El token que se desea validar.
+     * @return true si el token es válido, false en caso contrario.
+     */
     public static boolean isTokenValid(String token) {
         return Objects.equals(token, SECURITY_TOKEN);
     }
