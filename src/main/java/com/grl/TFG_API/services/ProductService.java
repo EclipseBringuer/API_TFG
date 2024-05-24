b86cee34afa4e15ffaa5cfb559045f71caf3ae00
gabri <gabrielrl2004@gmail.com>
2024-05-24 18:16:01 +0200

    Servicio de product comentado con javadoc

--- src/main/java/com/grl/TFG_API/services/ProductService.java
+++ src/main/java/com/grl/TFG_API/services/ProductService.java
@@ -6,19 +6,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+/**
+ * Servicio para la gestión de productos.
+ */
 @Service
 public class ProductService {
     private final ProductRepository repository;
 
+    /**
+     * Constructor del servicio ProductService.
+     *
+     * @param repository El repositorio de productos.
+     */
     @Autowired
     public ProductService(ProductRepository repository) {
         this.repository = repository;
     }
 
+    /**
+     * Obtiene todos los productos.
+     *
+     * @return Una lista de todos los productos.
+     */
     public List<Product> getAllProducts() {
         return repository.findAll();
     }
 
+    /**
+     * Obtiene todas las categorías distintas de productos.
+     *
+     * @return Una lista de todas las categorías distintas.
+     */
     public List<String> getAllCategories() {
         return repository.getDistinctCategories();
     }
