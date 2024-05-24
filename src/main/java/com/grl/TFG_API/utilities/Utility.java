a80d38a422b83f6f4e136407da174a8457634479
gabri <gabrielrl2004@gmail.com>
2024-05-24 18:06:19 +0200

    Utility con método para comentar no deprecado y mejorado

--- src/main/java/com/grl/TFG_API/utilities/Utility.java
+++ src/main/java/com/grl/TFG_API/utilities/Utility.java
@@ -8,10 +8,15 @@ import java.io.IOException;
 public class Utility {
     /**
      * Abre la documentación en un navegador web.
+     * <p>
+     * Actualmente este método no es usado ya que al subirlo el railway
+     * provoca una excepción en el despliegue por la configuración de los servidores.
+     * Sin embargo en local se puede llamar y funciona perfectamente, desplegando swagger.
      */
     public static void openDocumentation() {
         try {
-            Runtime.getRuntime().exec("cmd /c start chrome http://localhost:8080/docu");
+            new ProcessBuilder("cmd", "/c", "start", "chrome", "http://localhost:8080/docu")
+                    .start();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
